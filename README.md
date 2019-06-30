e87e66ef1b53db862be9ddae61b6d53d957fc9f2
doctor700 <52350838+doctor700@users.noreply.github.com>
2019-06-30 03:27:58 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# systm500
+# system
+system
---
+++ config.js.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NTczNjkwMzAxMDg5MDU0NzMw.XODH2w.SCTbWFSs4CEUIG0YLRhEhgIHfp4',    
+    YT_API_KEY: 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8 ',
+    prefix: '+',
+   devs: ['414555439536275459']
+}
