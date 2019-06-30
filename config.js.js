e6bee20e075ad24b34e9cdab3cea9d75c9201ed6
doctor700 <52350838+doctor700@users.noreply.github.com>
2019-06-30 06:30:39 +0200

    Add files via upload

--- config.js.js
+++ config.js.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'NTczNjkwMzAxMDg5MDU0NzMw.XODH2w.SCTbWFSs4CEUIG0YLRhEhgIHfp4',    
+    TOKEN: 'NTk0NzQ1MzU4NTkzMDMyMjE1.XRg5zA.QbQKCKqeAeINikrTmqbR3ECdMfk',    
     YT_API_KEY: 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8 ',
-    prefix: '+',
-   devs: ['414555439536275459']
+    prefix: '!',
+   devs: ['573517050241351691']
 }
